c6d00472729fcaccc16964ea8a08039b7b609a60
jesperringhog <jesper.ringhog@gmail.com>
2025-11-24 14:17:24 +0100

    add type models

---
+++ src/models/Movie.ts
@@ -0,0 +1,5 @@
+export type Movie = {
+    Title: string;
+    Year: string;
+    Poster: string;
+}
---
+++ src/models/OmdbResponse.ts
@@ -0,0 +1,6 @@
+import type { Movie } from "./movie"
+
+
+export type OmdbResponse = {
+    Search: Movie[];
+}
